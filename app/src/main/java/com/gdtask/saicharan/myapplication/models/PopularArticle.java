437cab9aeeae609bf22f3b127862a515a41d9847
SaiCharan <saicharanlanka4@gmail.com>
2019-02-21 12:44:30 +0530

    Only Recycler with direct URL

--- app/src/main/java/com/gdtask/saicharan/myapplication/models/PopularArticle.java
+++ app/src/main/java/com/gdtask/saicharan/myapplication/models/PopularArticle.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 public class PopularArticle {
     public String webUrl;
-
     public String imageUrl;
-
     public String title;
     public String tvabstract,tvdate;
 
@@ -24,12 +22,12 @@ public class PopularArticle {
                 p.tvdate = movieJsonResults.getJSONObject(i).getString("published_date");
                 p.webUrl = movieJsonResults.getJSONObject(i).getString("url");
                 JSONArray j = movieJsonResults.getJSONObject(i).getJSONArray("media");
-               /* if(j !=null && j.length() > 0) {
-                    JSONArray j1 = movieJsonResults.getJSONObject(0).getJSONArray("media-metadata");
+                if(j !=null && j.length() > 0) {
+                    JSONArray j1 = j.getJSONObject(0).getJSONArray("media-metadata");
                     if(j1!=null && j1.length() > 0) {
                         p.imageUrl = j1.getJSONObject(0).getString("url");
                     }
-                }*/
+                }
                 result.add(p);
             } catch (JSONException e) {
                 e.printStackTrace();
